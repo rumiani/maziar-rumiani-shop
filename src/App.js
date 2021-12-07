c6f84ce326dc68f14bd8e5ba9a44abaf5cbe1a64
maziar.rumiani <maziar.rumiani@gmail.com>
2021-12-08 01:43:38 +0330

    styled comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import react from 'react';
 import './App.css';
-
+import styled from 'styled-components'
+let StyledDiv = styled.div`
+background: blue;
+cursor:pointer;
+@media (min-width: 500px){
+  background: red;
+  
+}
+&:hover {
+  background: ${props => props.alt?'red':'black'};
+}
+`
 class App extends react.Component {
+  state = {
+    changeColor: false
+  }
+changeColorHandler = () => {
+  this.setState({changeColor: !this.state.changeColor})
+}
   render() { 
-    return <div>Maziar rumiani shop</div>;
+    return <StyledDiv alt={this.state.changeColor} onClick={this.changeColorHandler}>
+      Maziar rumiani shop
+      </StyledDiv>;
   }
 }
  
